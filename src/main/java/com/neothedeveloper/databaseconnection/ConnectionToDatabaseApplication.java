177411ad89a09be2a3ca6744f90b47cec0325755
Neo Prudence <neoboledi21@gmail.com>
2024-04-29 12:05:19 +0200

    First commit

---
+++ src/main/java/com/neothedeveloper/databaseconnection/ConnectionToDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.neothedeveloper.databaseconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionToDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionToDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neothedeveloper/databaseconnection/ConnectionToDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neothedeveloper.databaseconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionToDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
